2c8504f489080ed2279eb3d3926cf39c9649e296
Clara Diederichsen <clara.diederichsen@deepspace.nz>
2024-07-15 19:40:25 +0200

    Normalize all the line endings

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontSize: {
+        '18px': '18px',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
+
+
