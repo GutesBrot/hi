8d5cd4d9769a8ddffd7a0a40bc69a2b72b643017
Clara Diederichsen <clara.diederichsen@deepspace.nz>
2024-07-18 15:51:30 +0200

    Integrated clipart

--- react-app/src/components/About.js
+++ react-app/src/components/About.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import '../App.css'; // Correct path
+import '../App.css';
+import working from '../assets/clipart-work.webp';
 
 const About = () => {
     return (
         <section id="about" className="bg-intro py-16 pt-[120px] relative z-0">
             <div className="container mx-auto">
-                <p className='text-black'>Lorem ipsum dolor sit amet...</p>
+            <p className='text-black'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
             </div>
+            <img src={working} alt="Working Woman" className="working-woman-image pointer-events-none" />
         </section>
     );
 };
