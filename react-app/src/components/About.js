05176918df6a2a46220253e2b2eabeed9e52fc33
GutesBrot <clara.diederichsen@deepspace.nz>
2025-08-27 16:46:22 +0200

    Update to be more mobile friendly

--- react-app/src/components/About.js
+++ react-app/src/components/About.js
@@ -16,8 +16,8 @@ const About = () => {
 
     return (
         <section id="about" className="bg-intro py-16 pt-[120px] relative z-0">
-            <div className="container mx-auto">
-                <p className='text-black'>
+            <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+                <p className='text-black text-sm sm:text-base lg:text-lg leading-relaxed max-w-4xl mx-auto'>
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
                 </p>
             </div>
@@ -26,8 +26,8 @@ const About = () => {
                 <svg 
                     id="visual" 
                     viewBox="0 0 675 900" 
-                    width="675" 
-                    height="900" 
+                    width="100%" 
+                    height="100%" 
                     xmlns="http://www.w3.org/2000/svg" 
                     version="1.1">
                     <g transform="translate(337.5 450)">
